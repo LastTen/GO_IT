d9d039e78afd1cba265d90aaa264c4dc355d7d14
Roman Stas <romastas1996@gmail.com>
2024-04-13 11:11:47 +0300

    closure ++

---
+++ balance_closure.py
@@ -0,0 +1,26 @@
+def perents(coin: int) -> int:
+    """use when user buy something"""
+    coin_balans = coin  # Приймає "бюджет"
+
+    def child(lose):
+        nonlocal coin_balans  # виклик no local функції
+        coin_balans -= lose  # зміна бюджету на lose
+        return coin_balans
+
+    return child
+
+
+# Створення користувачів
+will = perents(100)  # Створення/Виклик функції для вілл
+sara = perents(100)  # виклик вже іншої функції для сара
+
+sell = input("Waste\n")
+
+sel_list = [int(i) for i in sell.split(",")]
+
+
+for i in sel_list:
+    if i % 2 == 0:
+        print(f"will balance {will(i)}")
+    else:
+        print(f"sara balance {sara(i)}")
