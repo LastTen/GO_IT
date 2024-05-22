4a4231733cb0092ba01345332f97e4fdda247db3
Roman Stas <romastas1996@gmail.com>
2024-05-22 19:24:45 +0300

    file reader

--- file_reader/cvs_reader.py
+++ file_reader/cvs_reader.py
@@ -26,6 +26,7 @@ file = "cvsfile.cvs"
 def write_contacts_to_file(filename, contacts):
     with open(filename, "w", newline="") as fh:
         field_names = [key for key in contacts[0]]
+        # field_names = contacts[0].keys()
         writer = csv.DictWriter(fh, fieldnames=field_names)
         writer.writeheader()
         for key in contacts:
